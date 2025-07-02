fee54bb39ed158ca63ca0228ce909d74df46c5b0
DurlabhDewangan <durlabhdewangan59@gmail.com>
2025-07-02 07:38:03 +0530

    add usermodel

--- src/index.js
+++ src/index.js
@@ -2,25 +2,19 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 import { app } from "./app.js";
 
-
-dotenv.config({ path: "./env" })
+dotenv.config({ path: "./.env" });
 
 connectDB()
-    .then(() => {
+  .then(() => {
 
-        const PORT = process.env.PORT || 8000;
+  const server = app.listen(PORT, () => {
+      console.log(` Server is running at port: ${PORT}`);
+    });
 
-        const server = app.listen(PORT, () => {
-            console.log(`Server is running at port: ${PORT}`);
-        });
-
-        server.on("error", (error) => {
-            console.log("ERR : ", error
-            );
-            throw error
-        })
-
-    })
-    .catch((err) => {
-        console.log("MONGO db connection failed !!! ", err);
-    })
+    server.on("error", (error) => {
+      console.error(" Server error:", error);
+      throw error;
+    });
+  }).catch((err)=>{
+    console.log("database connection failed", err);
+  }) 
---
+++ src/models/user.model.js
@@ -0,0 +1,104 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+    {
+username: {
+  type: String,
+  required: true,
+  unique: true,
+  lowercase: true,
+  trim: true,
+  index: true,
+  minlength: 3,
+  maxlength: 30,
+},
+
+ email:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+ },
+   phone: {
+  type: String,
+  required: true,
+  unique: true,
+  trim: true,
+ },
+
+ fullname:{
+    type:String,
+    required:true,
+    trim:true,
+ },
+  avatar:{
+    type:String,
+    default:null
+  },
+  role:{
+    type: String,
+    enum: ['customer', 'admin', 'staff'],
+    default: 'customer',
+  },
+  password:{
+    type:String,
+    required:[true, 'password is required']
+  },
+  refreshToken:{
+    type:String,
+    default:null,
+  }
+
+    }
+,{
+    timestamps:true
+}
+)
+
+userSchema.pre("save",  async function (next) {
+  if(this.isModified("password")){  this.password = await bcrypt.hash(this.password, 10)
+  next()}
+else{
+  return next()
+}  
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+ return await bcrypt.compare(password, this.password)
+  
+}
+
+userSchema.method.generateAccessToken = function(){
+ return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username:this.username,
+    fullname:this.fullname,
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn:  process.env.ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+userSchema.method.generateRefreshToken = function()
+{
+ return jwt.sign({
+    _id:this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn:  process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
+
+}
+
+
+
+
+export const User = mongoose.model("User" , userSchema)
